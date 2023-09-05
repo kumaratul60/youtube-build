20ae499ff9616397d6394f89eab3f5087c7fb8aa
Atul <atulreso1@gmail.com>
2023-09-05 15:41:02 +0530

    update config

--- src/config/constantAPI.js
+++ src/config/constantAPI.js
@@ -1,6 +1,9 @@
+const GOOGLE_API_KEY = "AIzaSyBQRyqykU6NycYaiHRgjUIIG5OIRIsg_Pw";
+
 export const YOUTUBE_VIDEOS_API =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
-  process.env.REACT_APP_GOOGLE_API_KEY;
+  GOOGLE_API_KEY;
+//  process.env.REACT_APP_GOOGLE_API_KEY
 
 export const YOUTUBE_SEARCH_API =
   "https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
